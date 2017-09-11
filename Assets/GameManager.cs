36e311f54ff853378faf3e7442856943dfa38b92
nikolalkc <nikolalkc@hotmail.com>
2017-09-11 23:39:41 +0200

    prelazi izmedju levela, mehanika za ruke osnovna

---
+++ Assets/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+	public GameObject leftHand, rightHand;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//leva ruka
+		if (Input.GetMouseButtonDown(0)) {
+			if (leftHand.activeInHierarchy) {
+				leftHand.SetActive(false);
+			}
+			else{
+				leftHand.SetActive(true);
+			}
+		}
+
+		//desna ruka
+		if (Input.GetMouseButtonDown(1)) {
+			if (rightHand.activeInHierarchy) {
+				rightHand.SetActive(false);
+			}
+			else{
+				rightHand.SetActive(true);
+			}
+		}
+	}
+}
---
+++ Assets/LoadAnotherLevel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadAnotherLevel : MonoBehaviour {
+	public int scene_index;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	void OnTriggerEnter(Collider col) {
+		SceneManager.LoadScene (scene_index);
+	}
+}
