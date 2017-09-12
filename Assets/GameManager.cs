1b4b018fc582ce20dd2c053b198c0b79af7d9c1b
nikolalkc <nikolalkc@hotmail.com>
2017-09-12 22:55:35 +0200

    efekti vizuelni

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,12 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class GameManager : MonoBehaviour {
 	public GameObject leftHand, rightHand;
 	// Use this for initialization
 	void Start () {
-		
 	}
 	
 	// Update is called once per frame
