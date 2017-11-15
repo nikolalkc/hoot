e64804df83e8d057d5b4ad950bb7221563d58930
nikolalkc <nikolalkc@hotmail.com>
2017-11-15 01:49:51 +0100

    sistem taskova zapocet

--- Assets/Content/Scripts/GameManager.cs
+++ Assets/Content/Scripts/GameManager.cs
@@ -2,17 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 	public GameObject leftHand, rightHand;
+	public Text Objective;
+	public string[] objectives;
+	public int objective_index = 0;
+	Collider CurrentObjectiveCollider;
 	// Use this for initialization
 	void Start () {
+		Objective.text = objectives[objective_index];
 	}
+
 	
 	// Update is called once per frame
 	void Update () {
 		//leva ruka
-		if (Input.GetMouseButtonDown(0)) {
+		/*if (Input.GetMouseButtonDown(0)) {
 			if (leftHand.activeInHierarchy) {
 				leftHand.SetActive(false);
 			}
@@ -30,5 +37,48 @@ public class GameManager : MonoBehaviour {
 				rightHand.SetActive(true);
 			}
 		}
+		*/
+
+		if (leftHand.activeSelf) {
+			if (Input.GetMouseButtonDown (0)) {
+				CompleteActiveTask ();
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider col) {
+		//print (col.tag);
+		if (col.tag == "task") {
+			SetHandsActive (true);
+		}
+		CurrentObjectiveCollider = col;
+	}
+
+	void OnTriggerExit(Collider col) {
+		if (col.tag == "task") {
+			SetHandsActive (false);
+		}
+		CurrentObjectiveCollider = null;
+	}
+
+	void CompleteActiveTask() {
+		Objective.text = "COMPLETED";
+		SetHandsActive (false);
+		CurrentObjectiveCollider.gameObject.GetComponent<GenericTask> ().PlayTaskSound ();
+		CurrentObjectiveCollider.enabled = false;
+
+	}
+
+
+	void SetHandsActive(bool state) {
+		if (state) {
+			leftHand.SetActive (true);
+			rightHand.SetActive (true);	
+		} else {
+			leftHand.SetActive(false);
+			rightHand.SetActive(false);
+		}
+
+	
 	}
 }
---
+++ Assets/Content/Scripts/GenericTask.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericTask : MonoBehaviour {
+	public int Index;
+	public bool isEnabled;
+	public AudioSource task_fx_source;
+	public AudioClip sfx;
+	// Use this for initialization
+	void Start () {
+		task_fx_source = gameObject.AddComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void PlayTaskSound() {
+
+		if (sfx) {
+			task_fx_source.PlayOneShot (sfx);
+		}
+	}
+}
